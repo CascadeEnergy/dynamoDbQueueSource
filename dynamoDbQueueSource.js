54c0566cc269730295fe6d531110357fed30ffc9
Charles Berube <charles.berube@cascadeenergy.com>
2015-01-21 14:37:57 -0800

    Add support for Query as well as Scan

--- dynamoDbQueueSource.js
+++ dynamoDbQueueSource.js
@@ -7,45 +7,49 @@ var dynamoDb = new AWS.DynamoDB();
 
 var dynamoDbQueueSource = { };
 
-dynamoDbQueueSource._processScanResult = function(err, data, scanTask) {
+dynamoDbQueueSource._processResult = function(err, data, task) {
   // Did we have an error? If so, pass it through to the callback, indicate
   // that we should stop iterating, and return
   if (err) {
-    scanTask.error = err;
-    scanTask.callback(err);
+    task.error = err;
+    task.callback(err);
     return;
   }
 
   // Push the items from the scan into the queue and update the total item count
-  scanTask.queue.push(data.Items);
-  scanTask.results.itemCount += data.Items.length;
+  task.queue.push(data.Items);
+  task.results.itemCount += data.Items.length;
 
   // Are we done? If so, trigger the callback for the scan and return
   if (!data.LastEvaluatedKey) {
-    scanTask.hasMoreItems = false;
-    scanTask.callback(null, scanTask.results);
+    task.hasMoreItems = false;
+    task.callback(null, task.results);
     return;
   }
 
   // If we have more rows to scan, repeat the scan with the new exclusive
   // start key
-  scanTask.options.ExclusiveStartKey = data.LastEvaluatedKey;
-  setImmediate(_.partial(dynamoDbQueueSource._executeScanTask, scanTask));
+  task.options.ExclusiveStartKey = data.LastEvaluatedKey;
+  setImmediate(task.executor);
 };
 
 dynamoDbQueueSource._executeScanTask = function(scanTask) {
   dynamoDb.scan(scanTask.options, scanTask.processor);
 };
 
+dynamoDbQueueSource._executeQueryTask = function(queryTask) {
+  dynamoDb.query(queryTask.options, queryTask.processor);
+};
+
 dynamoDbQueueSource._isRunning = function(scanTask) {
   return (
-  !scanTask.error &&
-  (scanTask.queue.length > 0 || scanTask.hasMoreItems)
+    !scanTask.error &&
+    (scanTask.queue.length > 0 || scanTask.hasMoreItems)
   );
 };
 
-dynamoDbQueueSource.scanToQueue = function(queue, options, callback) {
-  var scanTask = {
+dynamoDbQueueSource._createTask = function(queue, options, callback, executor) {
+  var task = {
     queue: queue,
     options: options,
     callback: callback,
@@ -57,20 +61,41 @@ dynamoDbQueueSource.scanToQueue = function(queue, options, callback) {
   // if we ensure that a function is always available as the callback, so we
   // just use the identity function from lodash if we didn't get a real
   // callback function
-  if (!_.isFunction(scanTask.callback)) {
-    scanTask.callback = _.identity;
+  if (!_.isFunction(task.callback)) {
+    task.callback = _.identity;
   }
 
-  scanTask.processor = _.partialRight(
-    dynamoDbQueueSource._processScanResult,
-    scanTask
+  task.processor = _.partialRight(dynamoDbQueueSource._processResult, task);
+  task.executor = _.partialRight(executor, task);
+  task.isRunning = _.partial(dynamoDbQueueSource._isRunning, task);
+
+  return task;
+};
+
+dynamoDbQueueSource.scanToQueue = function(queue, options, callback) {
+  var scanTask = dynamoDbQueueSource._createTask(
+    queue,
+    options,
+    callback,
+    dynamoDbQueueSource._executeScanTask
   );
 
-  scanTask.isRunning = _.partial(dynamoDbQueueSource._isRunning, scanTask);
-
-  dynamoDbQueueSource._executeScanTask(scanTask);
+  setImmediate(scanTask.executor);
 
   return scanTask;
+};
+
+dynamoDbQueueSource.queryToQueue = function(queue, options, callback) {
+  var queryTask = dynamoDbQueueSource._createTask(
+    queue,
+    options,
+    callback,
+    dynamoDbQueueSource._executeQueryTask
+  );
+
+  setImmediate(queryTask.executor);
+
+  return queryTask;
 };
 
 module.exports = dynamoDbQueueSource;
